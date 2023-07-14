e8c85564635806147712cc32b4ad9dd83345ed9b
Harobulus <52546682+Harobulus@users.noreply.github.com>
2023-07-14 10:47:46 +0100

    Coded a grocery shopping list

--- js/script.js
+++ js/script.js
@@ -18,3 +18,28 @@ var groceries = [
   "olive oil",
   "mustard"
 ];
+// Log out the length of the groceries variable
+console.log(groceries.length);
+
+// Create a variable called deleteDuplicates for a function expression with list as a parameter
+var deleteDuplicates = function(list) { 
+  var cleanList = [];
+  for (var item of list) { 
+    if (!cleanList.includes(item)) {
+      cleanList.push(item);
+    };
+  };
+  return cleanList;
+};
+
+console.log(deleteDuplicates(groceries));
+
+// Check the Length of the New Array
+var newGroceries = deleteDuplicates(groceries);
+console.log(newGroceries.length);
+
+// Create a Numbered List of the Grocery Items
+newGroceries.forEach(function (grocery, index) {
+  console.log(`#${index + 1} ${grocery}`);
+});
+
